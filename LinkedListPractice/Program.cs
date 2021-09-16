d80e481274085323cf6e164dedc4e7164e89e7cb
ka1peshc <kalpesh111997@gmail.com>
2021-09-17 00:00:22 +0530

    First commit

---
+++ LinkedListPractice/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LinkedListPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+This project is to learn linked list data type
