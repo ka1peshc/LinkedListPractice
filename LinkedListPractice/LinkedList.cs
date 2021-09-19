08015e0026a00d00020f3a5ea6eb9ccbd8c8c8fc
ka1peshc <kalpesh111997@gmail.com>
2021-09-19 21:12:50 +0530

    PopHead function to change head to next node as head

--- LinkedListPractice/LinkedList.cs
+++ LinkedListPractice/LinkedList.cs
@@ -7,9 +7,14 @@ namespace LinkedListPractice
     class LinkedList
     {
         internal Node head;
+        /// <summary>
+        /// Adding new node in linklist
+        /// </summary>
+        /// <param name="data"></param>
         internal void Add(int data)
         {
             Node node = new Node(data);
+            
             if (this.head == null)
             {
                 this.head = node;
@@ -24,9 +29,52 @@ namespace LinkedListPractice
                 temp.next = node;
             }
         }
-
+        /// <summary>
+        /// Add a node in particular position
+        /// </summary>
+        /// <param name="position"></param>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        internal Node AddInBetween(int position, int data)
+        {
+            //int index=1;
+            Node tempNode = this.head;
+            if (position < 1)
+            {
+                Console.WriteLine("invalid position");
+                
+            }
+            else if( position == 1)
+            {
+                Node newNode = new Node(data);
+                newNode.next = this.head;
+                head = newNode;
+            }
+            else
+            {
+                
+                while(position-- != 0)
+                {
+                    if(position == 1)
+                    {
+                        Node node = new Node(data);
+                        node.next = tempNode.next;
+                        tempNode.next = node;
+                        break;
+                    }
+                    tempNode = tempNode.next;
+                }
+                if(position != 1)
+                {
+                    Console.WriteLine("Position out of range");
+                }
+            }
+            Console.WriteLine("Inserted value is " + tempNode.data);
+            return tempNode;
+        }
         internal void Display()
         {
+            Console.WriteLine("head is :" + this.head.data);
             Node temp = this.head;
             if (temp == null)
             {
@@ -41,5 +89,12 @@ namespace LinkedListPractice
             Console.WriteLine("==============");
             
         }
+        internal void pophead()
+        {
+            Node tempNode = this.head;
+            head = tempNode.next;
+            Console.WriteLine("poped head");
+        }
+        
     }
 }
--- LinkedListPractice/Program.cs
+++ LinkedListPractice/Program.cs
@@ -8,8 +8,17 @@ namespace LinkedListPractice
         {
             LinkedList linklist = new LinkedList();
             linklist.Add(56);
-            linklist.Add(30);
+            linklist.Add(96);
+            //linklist.Add(30);
             linklist.Add(70);
+            linklist.Display();
+            linklist.AddInBetween(2, 30);
+            linklist.Display();
+            linklist.AddInBetween(3, 100);
+            linklist.Display();
+            linklist.pophead();
+            linklist.Display();
+            linklist.popTailNode();
             linklist.Display();
         }
     }
